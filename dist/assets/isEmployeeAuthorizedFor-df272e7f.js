79a23f0b3bd59ba42befc5dce83c64bd1c3b7966
KareemWebDeveloper <kemoayman2015@gmail.com>
2023-12-03 03:19:57 +0200

    deploying v2

---
+++ dist/assets/isEmployeeAuthorizedFor-df272e7f.js
@@ -0,0 +1,1 @@
+function l(e,r,i){return i=="admin"?!0:e.filter(t=>t.role_name==r).length>0}export{l as i};
---
+++ dist/assets/privateSubscriptionUpdate-b8270c96.js
@@ -0,0 +1,1 @@
+import{a0 as e,a as c,c as t}from"./index-54b3e273.js";const r={};function a(n,o){return c(),t("div")}const _=e(r,[["render",a]]);export{_ as default};
